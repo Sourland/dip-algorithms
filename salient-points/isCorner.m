30cec718353a029b8c2891cfaa81011b63f0c74e
sourland <jem2010@windowslive.com>
2022-05-04 21:38:28 +0300

    add other approach

--- salient-points/isCorner.m
+++ salient-points/isCorner.m
@@ -1,10 +1,15 @@
-function c = isCorner(I, p, k, Rthres)
+function [c, R] = isCorner(M, p, k, Rthres)
 %ISCORNER returns TRUE if pixel is on an edge corner
 %   @param I the image
 %   @param p the pixel for examination
 %   @param k a hyperparameter to calculate R
 %   @param Rthresh the minimum R for p to be in a corner
     
+    R = det(M) - k*trace(M).^2;
+    c = false;
+    if R > Rthresh
+        c = true;
+    end
     
 end
 
--- salient-points/myDetectHarrisFeatures.m
+++ salient-points/myDetectHarrisFeatures.m
@@ -1,29 +1,38 @@
 function corners = myDetectHarrisFeatures(I)
 %MYDETECTHARRISFEATURES searches and locates sallient points in an image
 %   @param I the image
-    I = rgb2gray(I);
-    [X, Y] = size(I);
+    img = rgb2gray(I);
+    [X, Y] = size(img);
     dx =[-1 0 1];
     dy = dx'; 
-    Ix = conv2(double(I), dx, 'same');
-    Iy = conv2(double(I), dy, 'same');
-    sigma = 0.5;
-    h = fspecial('gaussian', fix(sigma*6), sigma);
-    Ix2 = Ix.^2;
-    Iy2 = Iy.^2;
-    Ixy = Ix.^Iy;
-    k = 0.04;
-    Rtresh = 1e3;
-    gradientImage = cat(3,cat(3,Ix2, Iy2), Ixy); 
+    Ix = conv2(double(img), dx, 'same');
+    Iy = conv2(double(img), dy, 'same');
+    sigma = 2;
+    GaussianMask = fspecial('gaussian', 9, sigma);
     
-    for x = 1:X;
-        for y = 1:Y
-            p = [x y];
-            if isCorner(gradientImage, p, k, Rtresh)
-            
+    Ix2 = conv2(double(Ix.^2), GaussianMask,'same');
+    Iy2 = conv2(double(Iy.^2), GaussianMask,'same');
+    Ixy = conv2(double(Ix.*Iy), GaussianMask, 'same');
+    
+    k = 0.04;
+    Rthres = 1e3;
+    LayersOfFilteredImages = cat(3,cat(3,Ix2, Iy2), Ixy); 
+    E = zeros(X,Y);
+    corners = [];
+    for x = 2:1:X-1
+        for y = 2:1:Y-1
+            x_sum = sum(Ix2(x-1:x+1, y-1:y+1), 'all');
+            y_sum = sum(Iy2(x-1:x+1, y-1:y+1), 'all');
+            xy_sum = sum(Ixy(x-1:x+1, y-1:y+1), 'all');
+            M = [x_sum xy_sum;xy_sum y_sum];
+            E(x, y) = min(eig(M));
+            [found_corner, R] = isCorner(M, k, Rthres);
+            if found_corner
+                corner.location = p;
             end
+                
+                
         end
     end
-    
 end
 
